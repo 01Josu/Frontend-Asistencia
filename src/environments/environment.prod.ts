0fe03c37cd253c9485dbb604074bcc1b392758cc
Josue Rojas <jr045514@gmail.com>
2026-01-15 14:56:24 -0500

    fix: add /api to prod base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-asistencia-production-4a9b.up.railway.app'
+  apiUrl: 'https://backend-asistencia-production-4a9b.up.railway.app/api'
 };
